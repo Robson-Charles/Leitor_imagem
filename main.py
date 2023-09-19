830ae79bc9deadb8f39caee2b84f2c1d0ec6c78c
robson charles <robsoncharles.jobs@gmail.com>
2023-09-19 17:56:29 -0300

    colocando novas depedencias

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile
 from Converter_imagem import escrever_arquivo
 app = FastAPI()
 from fastapi.responses import JSONResponse
+import uvicorn
 @app.post("/Lucas")
 async def root(file: UploadFile):
     #print(file.file.read().decode('utf-8'))
@@ -9,3 +10,5 @@ async def root(file: UploadFile):
     texto = escrever_arquivo.criar_txt(file.file.read().decode('utf-8'))
     print(texto)
     return JSONResponse(content={"mensagem": texto}, status_code=200)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
