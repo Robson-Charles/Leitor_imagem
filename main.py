f9c3fb8860b4785676966b66cb355bd122777388
Robson-Charles <98132158+Robson-Charles@users.noreply.github.com>
2023-10-04 18:32:57 -0300

    resovendo o bug da main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from Converter_imagem import escrever_arquivo
-import os
-
 app = Flask(__name__)
+import os
+@app.route
+def get_texto(svg):
+    return jsonify(escrever_arquivo.criar_txt(svg))
 api_keys = {
     'client1': 'rob',
 
 }
+# app.py
+
+#from process_file import process_text_file
+
+app = Flask(__name__)
+
 @app.route('/captcha', methods=['POST'])
 def process():
     api_key = request.headers.get('X-API-Key')  # Obtenha a chave da solicitação
 
     if api_key not in api_keys.values():
         return jsonify({'error': 'Chave de API inválida'}), 401
-
     # Verifica se o arquivo de texto foi enviado como parâmetro
+    if 'file' not in request.files:
+        return jsonify({'error': 'Arquivo não especificado'}), 400
+
     file = request.files['file']
+
+    # Verifique se o nome do arquivo é vazio
+    if file.filename == '':
+        return jsonify({'error': 'Nome de arquivo vazio'}), 400
+
     try:
         # Salve o arquivo no sistema de arquivos (opcional)
         # O exemplo a seguir salva o arquivo no diretório de upload
@@ -26,16 +41,15 @@ def process():
         file_path = os.path.join(upload_dir, file.filename)
         file.save(file_path)
 
-        # Chame a função para processar o arquivo (substitua 'escrever_arquivo' pela função apropriada)
-        result = escrever_arquivo.melhorar_font(file_path)
-        
+        # Chame a função para processar o arquivo e obter o resultado
+        result = get_texto(file_path)
+
         # Exclua o arquivo após o processamento, se necessário
         os.remove(file_path)
 
         return jsonify({'result': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
