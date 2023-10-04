b1394c1db21f49b62b4fb8dfbb18b76159a89904
Robson-Charles <98132158+Robson-Charles@users.noreply.github.com>
2023-10-04 07:48:04 -0300

    Add files via upload

--- Converter_imagem.py
+++ Converter_imagem.py
@@ -22,8 +22,8 @@ class Converter_imagem:
             img.format = 'jpg'
             img.save(filename='Captcha.jpg')
 
-        os.remove('Captcha.svg')
-        ler_texto_da_imagem.iniciar()
+        os.remove(self)
+        return ler_texto_da_imagem.iniciar()
 
 
 
@@ -40,7 +40,7 @@ class escrever_arquivo:
 
 
 
-    def __melhorar_font(self = ' '):
+    def melhorar_font(self = ' '):
 
         with open(self, 'r', encoding='UTF-8') as arq:
             arquivo = arq.read()
@@ -95,12 +95,12 @@ class escrever_arquivo:
             if '</svg>' not in svg_conferir_final:
                 with open('Captcha.txt', 'a', encoding='UTF-8') as arq:
                     arq.write('</svg>')
-        return manipular_arquivo._transforma_em_svg('Captcha.txt')
+        return manipular_arquivo._transforma_em_svg(self)
     def criar_txt(self):
-        #print(self)
-        #with open("Captcha.txt", 'w', encoding='UTF-8') as arq:
+        print(self)
+        with open("Captcha.txt", 'w', encoding='UTF-8') as arq:
 
-            #arq.write(self.replace('%', ' ').replace('*', '=').replace('width="300"', 'width="1600"').replace('height="100"', 'height="900"').replace('#', '').replace('><path', '>|<path'))
+            arq.write(self.replace('%', ' ').replace('*', '=').replace('width="300"', 'width="1600"').replace('height="100"', 'height="900"').replace('#', '').replace('><path', '>|<path'))
         return escrever_arquivo.__melhorar_font(self)
 
 
--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from flask import Flask, jsonify, request
 from Converter_imagem import escrever_arquivo
-#app = Flask(__name__)
-
-#@app.route
-#def get_texto(svg):
-    #return jsonify(Converter_imagem.escrever_arquivo.criar_txt(svg))
-# app.py
-#from flask import Flask, request, jsonify
-#from process_file import process_text_file
+import os
 
 app = Flask(__name__)
 api_keys = {
     'client1': 'rob',
 
 }
-@app.route('/captcha', methods=['GET'])
+@app.route('/captcha', methods=['POST'])
 def process():
     api_key = request.headers.get('X-API-Key')  # Obtenha a chave da solicitação
 
@@ -22,17 +15,27 @@ def process():
         return jsonify({'error': 'Chave de API inválida'}), 401
 
     # Verifica se o arquivo de texto foi enviado como parâmetro
-    file = request.args.get('file')
-
-    if not file:
-        return jsonify({'error': 'Arquivo não especificado'}), 400
-
+    file = request.files['file']
     try:
-        # Chama a função para processar o arquivo
-        result = escrever_arquivo.criar_txt(file)
+        # Salve o arquivo no sistema de arquivos (opcional)
+        # O exemplo a seguir salva o arquivo no diretório de upload
+        upload_dir = 'uploads'
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+
+        file_path = os.path.join(upload_dir, file.filename)
+        file.save(file_path)
+
+        # Chame a função para processar o arquivo (substitua 'escrever_arquivo' pela função apropriada)
+        result = escrever_arquivo.melhorar_font(file_path)
+        
+        # Exclua o arquivo após o processamento, se necessário
+        os.remove(file_path)
+
         return jsonify({'result': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
